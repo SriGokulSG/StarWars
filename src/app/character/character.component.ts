eba4317a6054fea1d3d7ba96a0ce35d6fa83d876
Sri Gokul <srigokul.sg@gmail.com>
2024-07-05 22:35:19 +0530

    removed console.log

--- src/app/character/character.component.ts
+++ src/app/character/character.component.ts
@@ -30,35 +30,35 @@ export class CharacterComponent {
   getCharacterDetails(characterId: number) {
     this.movieService.getCharacterDetails(characterId).subscribe(response => {
       this.details = response;
-      console.log(this.details)
+      // console.log(this.details)
       if (this.details.species.length == 0) {
         this.details.species = "Human"
       } else {
         this.movieService.fetchAPI(this.details.species[0]).subscribe(response => {
           this.details.species = response.name;
-          console.log(this.details)
+          // console.log(this.details)
         })
       }
       this.movieService.fetchAPI(this.details.homeworld).subscribe(response => {
         this.details.homeworld = response.name;
-        console.log(this.details)
+        // console.log(this.details)
       })
       this.details.films.forEach((element: string) => {
         this.movieService.fetchAPI(element).subscribe(response => {
           this.films.push(response);
-          console.log(this.films)
+          // console.log(this.films)
         })
       });
       this.details.vehicles.forEach((element: string) => {
         this.movieService.fetchAPI(element).subscribe(response => {
           this.vehicles.push(response);
-          console.log(this.vehicles)
+          // console.log(this.vehicles)
         })
       });
       this.details.starships.forEach((element: string) => {
         this.movieService.fetchAPI(element).subscribe(response => {
           this.spaceships.push(response);
-          console.log(this.spaceships)
+          // console.log(this.spaceships)
         })
       });
     })
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -66,37 +66,37 @@ export class HomeComponent {
   getPeople() {
     this.movieService.getPeopleList().subscribe(response => {
       this.movies = response.results;
-      console.log(this.movies)
+      // console.log(this.movies)
 
       this.movieService.callAPI(2).subscribe(response => {
         this.movies = [...this.movies, ...response.results]
-        console.log(this.movies)
+        // console.log(this.movies)
 
         this.movieService.callAPI(3).subscribe(response => {
           this.movies = [...this.movies, ...response.results]
-          console.log(this.movies)
+          // console.log(this.movies)
 
           this.movieService.callAPI(4).subscribe(response => {
             this.movies = [...this.movies, ...response.results]
-            console.log(this.movies)
+            // console.log(this.movies)
 
             this.movieService.callAPI(5).subscribe(response => {
               this.movies = [...this.movies, ...response.results]
-              console.log(this.movies)
+              // console.log(this.movies)
 
               this.movieService.callAPI(6).subscribe(response => {
                 this.movies = [...this.movies, ...response.results]
-                console.log(this.movies)
+                // console.log(this.movies)
 
                 this.movieService.callAPI(7).subscribe(response => {
                   this.movies = [...this.movies, ...response.results]
-                  console.log(this.movies)
+                  // console.log(this.movies)
                   this.movieService.callAPI(8).subscribe(response => {
                     this.movies = [...this.movies, ...response.results]
-                    console.log(this.movies)
+                    // console.log(this.movies)
                     this.movieService.callAPI(9).subscribe(response => {
                       this.movies = [...this.movies, ...response.results]
-                      console.log(this.movies)
+                      // console.log(this.movies)
 
                       this.character = this.movies;
                       this.getBirthYear();
@@ -119,7 +119,7 @@ export class HomeComponent {
         this.films.push({ name: element.title, value: element.url })
       });
     })
-    console.log(this.films)
+    // console.log(this.films)
   }
 
   getSpecies() {
@@ -151,7 +151,7 @@ export class HomeComponent {
     })
     this.species[''] = 'Human'
     this.specimen[''] = 'Human'
-    console.log(this.species)
+    // console.log(this.species)
   }
 
   getVehicles() {
@@ -177,7 +177,7 @@ export class HomeComponent {
         this.vehicles.push({ name: element.name, value: element.url })
       });
     })
-    console.log(this.vehicles)
+    // console.log(this.vehicles)
   }
 
   getstarships() {
@@ -203,7 +203,7 @@ export class HomeComponent {
         this.starships.push({ name: element.name, value: element.url })
       });
     })
-    console.log(this.starships)
+    // console.log(this.starships)
   }
 
   getBirthYear() {
@@ -212,10 +212,10 @@ export class HomeComponent {
       var mySubString = element.url.substring(element.url.length - 3)
       mySubString = mySubString.replace(/[^0-9]/g, "");
       element.id = mySubString
-      console.log(mySubString)
+      // console.log(mySubString)
     })
     this.birthYears = this.removeDuplicates(this.birthYears);
-    console.log(this.birthYears)
+    // console.log(this.birthYears)
   }
 
   removeDuplicates(array: any[]): any[] {
@@ -223,9 +223,9 @@ export class HomeComponent {
   }
 
   movieChange(event: any) {
-    console.log(event.target.value);
+    // console.log(event.target.value);
     if (this.filters.films.some((e: any) => e === event.target.value)) {
-      console.log("Hi")
+      // console.log("Hi")
       this.filters.films = this.filters.films.filter((filter: { value: any; }) => filter !== event.target.value);
     } else {
       this.filters.films.push(event.target.value)
@@ -234,9 +234,9 @@ export class HomeComponent {
   }
 
   speciesChange(event: any) {
-    console.log(event.target.value);
+    // console.log(event.target.value);
     if (this.filters.species.some((e: any) => e === event.target.value)) {
-      console.log("Hi")
+      // console.log("Hi")
       this.filters.species = this.filters.species.filter((filter: { value: any; }) => filter !== event.target.value);
     } else {
       this.filters.species.push(event.target.value)
@@ -247,7 +247,7 @@ export class HomeComponent {
   vehicleChange(event: any) {
     console.log(event.target.value);
     if (this.filters.vehicles.some((e: any) => e === event.target.value)) {
-      console.log("Hi")
+      // console.log("Hi")
       this.filters.vehicles = this.filters.vehicles.filter((filter: { value: any; }) => filter !== event.target.value);
     } else {
       this.filters.vehicles.push(event.target.value)
@@ -256,9 +256,9 @@ export class HomeComponent {
   }
 
   starShipChange(event: any) {
-    console.log(event.target.value);
+    // console.log(event.target.value);
     if (this.filters.starships.some((e: any) => e === event.target.value)) {
-      console.log("Hi")
+      // console.log("Hi")
       this.filters.starships = this.filters.starships.filter((filter: { value: any; }) => filter !== event.target.value);
     } else {
       this.filters.starships.push(event.target.value)
@@ -267,9 +267,9 @@ export class HomeComponent {
   }
 
   yearChange(event: any) {
-    console.log(event.target.value);
+    // console.log(event.target.value);
     if (this.filters.birth_year.some((e: any) => e === event.target.value)) {
-      console.log("Hi")
+      // console.log("Hi")
       this.filters.birth_year = this.filters.birth_year.filter((filter: { value: any; }) => filter !== event.target.value);
     } else {
       this.filters.birth_year.push(event.target.value)
@@ -277,23 +277,23 @@ export class HomeComponent {
     this.filterCharacters();
   }
   filterCharacters() {
-    console.log(this.filters)
+    // console.log(this.filters)
     this.movies = [];
     this.character.forEach((element: any) => {
       if (element['species'].length == 0) element['species'].push('https://swapi.dev/api/species/1/')
       var isOk = true;
       for (var key in this.filters) {
         this.filters[key].forEach((e: any) => {
-          console.log(e)
-          console.log(element[key])
-          console.log(element[key].includes(e))
+          // console.log(e)
+          // console.log(element[key])
+          // console.log(element[key].includes(e))
           if (!element[key].includes(e))
             isOk = false
         });
       }
       if (isOk)
         this.movies.push(element)
-      console.log(this.movies)
+      // console.log(this.movies)
       this.currentNumber=[]
     this.paginate(1);
     });
@@ -318,7 +318,7 @@ export class HomeComponent {
   paginate(pageNumber: number) {
     this.page = pageNumber,
     this.pages = Math.floor(this.movies.length % 5 == 0 ? this.movies.length / 5 : (this.movies.length / 5) + 1)
-    console.log(this.pages)
+    // console.log(this.pages)
     if(!this.currentNumber.includes(this.pages) || pageNumber<=this.pages-5){
       this.currentNumber = [];
       for(let i=pageNumber;i<pageNumber+5 && i<=this.pages;i++){
